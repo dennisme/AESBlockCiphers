a8a8cc18931034c250bec02dca57d7c0dbe1bd06
Matthew Dennison <medennison123@gmail.com>
2014-11-18 22:56:10 -0500

    template added

---
+++ template.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from blocks import aesECB
+
+# TODO
+"""This script will be used to send data to the various block ciphers.
+The idea is to make it really easy to encrypt and decrypt data.
+"""
